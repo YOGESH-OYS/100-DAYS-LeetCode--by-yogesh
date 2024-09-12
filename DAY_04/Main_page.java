61a4b944a0c8395fa026252168708aa714393fa0
Yogesh <longquest.oyscommunity@gmail.com>
2024-09-12 23:38:23 +0530

    DAY-04

--- DAY_04/Main_page.java
+++ DAY_04/Main_page.java
@@ -2,7 +2,7 @@ package DAY_04;
 
 public class Main_page {
     public static void main(String[] args) {
-        int k = 499979;
+        int k = 1000; // You can change this to any number, like 49000
         Solution l = new Solution();
         System.out.println(l.countPrimes(k));
     }
--- DAY_04/Solution_side.java
+++ DAY_04/Solution_side.java
@@ -2,20 +2,44 @@ package DAY_04;
 
 class Solution {
     public int countPrimes(int n) {
-        int count = 1,count2 = 0;
         if (n <= 2) {
             return 0;
         }
-        for (int i = 3; i < n; i++) {
-            if (i%2 != 0) {
-                for (int j = 2; j < i; j++) {
-                    if (i % j == 0) {break;}
-                    else {count2++;}
+        boolean[] a = new boolean[n];
+        for (int i = 2; i < n; i++) {
+            a[i] = true;
+        }
+
+        for (int i = 2; i*i < n; i++) {
+            if (a[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    a[j] = false;
                 }
-                if (count2 == i - 2) {count++;}
-                count2 = 0;
+            }
+        }
+
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (a[i]) {
+                count++;
             }
         }
         return count;
     }
 }
+
+//        int count = 1,count2 = 0;
+//        if (n <= 2) {
+//        return 0;
+//        }
+//        for (int i = 3; i < n; i++) {
+//        if (i%2 != 0) {
+//        for (int j = 2; j < i; j++) {
+//        if (i % j == 0) {break;}
+//        else {count2++;}
+//        }
+//        if (count2 == i - 2) {count++;}
+//count2 = 0;
+//        }
+//        }
+//        return count;
