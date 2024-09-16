16d57e6ee269799682c42a2fb5712361f55111ff
Yogesh <longquest.oyscommunity@gmail.com>
2024-09-16 20:43:23 +0530

    DAY-08

---
+++ DAY_08/Main_page.java
@@ -0,0 +1,9 @@
+package DAY_08;
+
+public class Main_page {
+    public static void main(String[] args) {
+        int[] a = {0,1,0,1,0,1,99};
+        Solution l = new Solution();
+        System.out.println(l.singleNumber(a));
+    }
+}
