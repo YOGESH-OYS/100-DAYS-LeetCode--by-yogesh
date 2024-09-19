27b2c90b4768a2fc59d95b4f4dcc5e3c64aa8672
Yogesh <longquest.oyscommunity@gmail.com>
2024-09-19 20:16:13 +0530

    DAY-10

---
+++ DAY_10/Main_page.java
@@ -0,0 +1,9 @@
+package DAY_10;
+
+public class Main_page {
+    public static void main(String[] args) {
+
+    }
+}
+
+//THIS PROGRAM WILL BE UPDATED SOON
---
+++ DAY_10/Solution_side.java
@@ -0,0 +1,6 @@
+package DAY_10;
+
+public class Solution_side {
+}
+
+//THIS PROGRAM WILL BE UPDATED SOON
