44ae55d4804ec3a5fff36589ad3dea49dac474a8
Yogesh <longquest.oyscommunity@gmail.com>
2024-09-19 20:11:03 +0530

    DAY-01

--- DAY_01/Main_page.java
+++ DAY_01/Main_page.java
@@ -3,6 +3,7 @@ package DAY_01;
 public class Main_page {
     public static void main(String[] args) {
         int[] a = {4,5,6,7,0,1,2};
+
         int target = 0;
         Solution m = new Solution();
         System.out.println(m.search(a,target));
