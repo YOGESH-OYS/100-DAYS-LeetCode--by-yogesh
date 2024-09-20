3983fc1040e89011373c929b86eb4bd659e6020f
Yogesh <longquest.oyscommunity@gmail.com>
2024-09-20 16:56:54 +0530

    DAY-12

---
+++ DAY_12/Main_page.java
@@ -0,0 +1,9 @@
+package DAY_12;
+
+public class Main_page {
+    public static void main(String[] args) {
+        Solution l = new Solution();
+        String s = "rat", t = "car";
+        System.out.println(l.isAnagram(s,t));
+    }
+}
